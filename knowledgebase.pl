6aab6954e1d5e493f5d334bcaa630d46eff8f107
Guruth <Kai.Weller90@googlemail.com>
2013-03-30 00:11:46 +0100

    working Auto Completion ? Working Auto Completion!

--- knowledgebase.pl
+++ knowledgebase.pl
@@ -8,3 +8,15 @@ Y=[
 ['Tschüs','Micha','bla',30],
 ['Tschüs','Bucks','bla',30]
 ].
+
+	findHighestTrigrams(Sentence , [
+    ['Hallo','Welt','was',10],
+    ['Hallo','Bucks','wie',20],
+    ['Hallo','Welt','wie',30],
+    ['Hallo','Bucks','wann',40],
+    ['Hallo','Micha','was',50],
+    ['Tschüs','Micha','bla',60],
+    ['Tschüs','Bucks','bla',70],
+    ['Hallo','Bucks','bla',40]
+    ] , PredictedWord),
+    findHighestProp(PredictedWord,HighestPropWord).
--- plague.pl
+++ plague.pl
@@ -1,63 +1,69 @@
-use_module(library(wgraphs)).
+autoComplete(Sentence,  Return):-
+    openBigrams(BigramHandle),
+    openTrigrams(TrigramHandle),
+    findBigram(BigramHandle,Sentence,Bigram),
+    findTrigrams(TrigramHandle,Sentence,Trigrams),
+    calculateProps(Bigram, Trigrams, ReturnWords),
+    findHighestProp(ReturnWords,Return).
 
+% Handling large lists of data http://www.swi-prolog.org/pldoc/package/table.html
 
+openBigrams(Handle):-
+    new_table(
+        'wordlists/w2_.txt', 
+        [count(integer), word(string), word2(string)], 
+        [],
+        Handle).
+openTrigrams(Handle):-
+    new_table(
+        'wordlists/w3_.txt', 
+        [count(integer), word(string), word2(string), word3(string)], 
+        [],
+        Handle).
 
-getLines(Lines) :-
-    open('bla.txt', read, Str),
-    read_file(Str,Lines),
-    close(Str).
+findBigram(Handle,[Word,Word2], Return):-
+    findall(
+        [Word, Word2, Count],
+        in_table(Handle, [word(Word), word2(Word2), count(Count)], _),
+        Return).
 
-read_file(Stream,[]) :-
-    at_end_of_stream(Stream).
+findTrigrams(Handle,[Word,Word2], ReturnList):-
+    findall(
+        [Word, Word2, Word3, Count], 
+        in_table( Handle, [word(Word), word2(Word2), word3(Word3),count(Count)], _), 
+        ReturnList).
 
-read_file(Stream,[X|L]) :-
-    \+ at_end_of_stream(Stream),
-    read(Stream,X),
-    write(X),
-    read_file(Stream,L).
+calculateProps(Words,[],[]).
+calculateProps([[BiWord,BiWord2,BiCount]],[[TriWord,TriWord2,TriWord3,TriCount]| TriList],[[TriWord3,Prop]|ReturnList]):-
+   calculateProps([[BiWord,BiWord2,BiCount]],TriList,ReturnList),
+    Prop is TriCount/BiCount.
 
-%% Format Dict = [[Wort, Wahrscheinlichkeit],...]
-				%Dict
-lookup(Sentence,  HighestPropWord):-
-%	reverse(Sentence, ReversedSentence),
-	findTrigrams(Sentence , [
-    ['Hallo','Welt','was',10],
-    ['Hallo','Bucks','wie',20],
-    ['Hallo','Welt','wie',30],
-    ['Hallo','Bucks','wann',40],
-    ['Hallo','Micha','was',50],
-    ['Tschüs','Micha','bla',60],
-    ['Tschüs','Bucks','bla',70],
-    ['Hallo','Bucks','bla',30]
-    ] , PredictedWord),
-    findHighestProp(PredictedWord,HighestPropWord).
-
-%						 Liste mit zwei Wörtern, werden über Console mit lookup(['Blaa','Blub'], ...) rein gereicht
-%									Dictionary mit Trigrammen sowie deren Wahrscheinlichkeit [[1,2,3,Prop],[1, 2,3,Prop],...]
-%																		Return Liste  mit möglichen Wörtern so wie deren Wahrscheinlichkeit 
-findTrigrams(WordList,[],[]).
-findTrigrams([Word, Word2], [[Word,Word2|RestDictEntry]|Dict], [RestDictEntry|ReturnList]):-
-	findTrigrams([Word,Word2], Dict, ReturnList).
-findTrigrams([Word,Word2|RestWord], [[DictWord,DictWord2|RestDictEntry]|Dict], ReturnList):-
-	findTrigrams([Word,Word2], Dict, ReturnList).
-%findTrigrams([Word,Word2|RestWord], [[DictWord,Word2|RestDictEntry]|Dict], ReturnList):-
-%	findTrigrams([Word,Word2], Dict, ReturnList).
-
-
-findBigrams([Word],[],[]).
-findBigrams([Word], [[Word|RestDictEntry]|Dict], [RestDictEntry|ReturnList]):-
-	findBigrams([Word], Dict, ReturnList).
-findBigrams([Word],[[DictWord|DictWordRest]| Dict],ReturnList):-
-	findBigrams([Word], Dict, Returnlist).
-%['Hallo'-['Welt'],['Bucks'-['wie']]]
 
 findHighestProp([Word],Word).
 findHighestProp([Word|RestList], Return):-
     findHighestProp(RestList,Rec),
-    compareWords(Word,Rec,Return).
+    compareWords(Word,Rec,Return). 
 
 compareWords([Word|Prop],[CompWord,Prop],[Word|Prop]).
 compareWords([Word,Prop],[CompWord,CompProp],[ReturnWord,ReturnProp]):-
     (Prop > CompProp ->
     ReturnWord = Word, ReturnProp = Prop ;
     ReturnWord = CompWord, ReturnProp = CompProp ).
+
+%						 Liste mit zwei Wörtern, werden über Console mit lookup(['Blaa','Blub'], ...) rein gereicht
+%									Dictionary mit Trigrammen sowie deren Wahrscheinlichkeit [[1,2,3,Prop],[1, 2,3,Prop],...]
+%																		Return Liste  mit möglichen Wörtern so wie deren Wahrscheinlichkeit 
+findHighestTrigrams(WordList,[],[]).
+findHighestTrigrams([Word, Word2], [[Word,Word2|RestDictEntry]|Dict], [RestDictEntry|ReturnList]):-
+	findHighestTrigrams([Word,Word2], Dict, ReturnList).
+findHighestTrigrams([Word,Word2|RestWord], [[DictWord,DictWord2|RestDictEntry]|Dict], ReturnList):-
+	findHighestTrigrams([Word,Word2], Dict, ReturnList).
+%findTrigrams([Word,Word2|RestWord], [[DictWord,Word2|RestDictEntry]|Dict], ReturnList):-
+%	findTrigrams([Word,Word2], Dict, ReturnList).
+
+
+findHighestBigrams([Word],[],[]).
+findHighestBigrams([Word], [[Word|RestDictEntry]|Dict], [RestDictEntry|ReturnList]):-
+	findHighestBigrams([Word], Dict, ReturnList).
+findHighestBigrams([Word],[[DictWord|DictWordRest]| Dict],ReturnList):-
+	findHighestBigrams([Word], Dict, Returnlist).
