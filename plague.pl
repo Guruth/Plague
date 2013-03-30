e3ce18110b2d97b795a860f9adad95c52cc9143e
Guruth <Kai.Weller90@googlemail.com>
2013-03-30 02:34:21 +0100

    Yaaay Console Input

--- plague.pl
+++ plague.pl
@@ -1,9 +1,30 @@
 % Handling large lists of data http://www.swi-prolog.org/pldoc/package/table.html
 % Word lists from : http://www.ngrams.info/
 % This could be a little bit overpowered : http://storage.googleapis.com/books/ngrams/books/datasetsv2.html
-autoComplete(Sentence,  Return):-
+
+startAutoComplete:-
     openBigrams(BigramHandle),
     openTrigrams(TrigramHandle),
+    read(Word),
+    inputLoop(Word,Word,BigramHandle,TrigramHandle).
+
+inputLoop(Sentence, OldWord, BigramHandle, TrigramHandle):-
+    read(NewWord),
+    autoComplete([OldWord, NewWord], BigramHandle, TrigramHandle, AutoCompletedWord),
+    string_concat(Sentence, ' ', NewSentence),
+    string_concat(NewSentence, NewWord, TotalyNewSentence),
+    write(TotalyNewSentence),write(AutoCompletedWord),nl,
+    inputLoop(TotalyNewSentence, NewWord, BigramHandle, TrigramHandle).
+
+autoComplete(Sentence, Return):-
+    openBigrams(BigramHandle),
+    openTrigrams(TrigramHandle),
+    findBigram(BigramHandle,Sentence,Bigram),
+    findTrigrams(TrigramHandle,Sentence,Trigrams),
+    calculateProps(Bigram, Trigrams, ReturnWords),
+    findHighestProp(ReturnWords,Return).
+
+autoComplete(Sentence, BigramHandle, TrigramHandle, Return):-
     findBigram(BigramHandle,Sentence,Bigram),
     findTrigrams(TrigramHandle,Sentence,Trigrams),
     calculateProps(Bigram, Trigrams, ReturnWords),
@@ -34,37 +55,39 @@ findTrigrams(Handle,[Word,Word2], ReturnList):-
         in_table( Handle, [word(Word), word2(Word2), word3(Word3),count(Count)], _), 
         ReturnList).
 
-calculateProps(Words,[],[]).
-calculateProps([[BiWord,BiWord2,BiCount]],[[TriWord,TriWord2,TriWord3,TriCount]| TriList],[[TriWord3,Prop]|ReturnList]):-
+calculateProps(_Sentence,[],[]).
+calculateProps([[BiWord,BiWord2,BiCount]],[[_TriWord, _TriWord2, TriWord3,TriCount]| TriList],[[TriWord3,Prop]|ReturnList]):-
    calculateProps([[BiWord,BiWord2,BiCount]],TriList,ReturnList),
     Prop is TriCount/BiCount.
-
 
 findHighestProp([Word],Word).
 findHighestProp([Word|RestList], Return):-
     findHighestProp(RestList,Rec),
     compareWords(Word,Rec,Return). 
 
-compareWords([Word|Prop],[CompWord,Prop],[Word|Prop]).
+compareWords([Word|Prop],[_CompWord,Prop],[Word|Prop]).
 compareWords([Word,Prop],[CompWord,CompProp],[ReturnWord,ReturnProp]):-
-    (Prop > CompProp ->
+    (>(Prop,CompProp) ->
     ReturnWord = Word, ReturnProp = Prop ;
     ReturnWord = CompWord, ReturnProp = CompProp ).
+
+
+
 
 %						 Liste mit zwei Wörtern, werden über Console mit lookup(['Blaa','Blub'], ...) rein gereicht
 %									Dictionary mit Trigrammen sowie deren Wahrscheinlichkeit [[1,2,3,Prop],[1, 2,3,Prop],...]
 %																		Return Liste  mit möglichen Wörtern so wie deren Wahrscheinlichkeit 
-findHighestTrigrams(WordList,[],[]).
-findHighestTrigrams([Word, Word2], [[Word,Word2|RestDictEntry]|Dict], [RestDictEntry|ReturnList]):-
-	findHighestTrigrams([Word,Word2], Dict, ReturnList).
-findHighestTrigrams([Word,Word2|RestWord], [[DictWord,DictWord2|RestDictEntry]|Dict], ReturnList):-
-	findHighestTrigrams([Word,Word2], Dict, ReturnList).
+%findHighestTrigrams(WordList,[],[]).
+%findHighestTrigrams([Word, Word2], [[Word,Word2|RestDictEntry]|Dict], [RestDictEntry|ReturnList]):-
+%	findHighestTrigrams([Word,Word2], Dict, ReturnList).
+%findHighestTrigrams([Word,Word2|RestWord], [[DictWord,DictWord2|RestDictEntry]|Dict], ReturnList):-
+%	findHighestTrigrams([Word,Word2], Dict, ReturnList).
 %findTrigrams([Word,Word2|RestWord], [[DictWord,Word2|RestDictEntry]|Dict], ReturnList):-
 %	findTrigrams([Word,Word2], Dict, ReturnList).
 
 
-findHighestBigrams([Word],[],[]).
-findHighestBigrams([Word], [[Word|RestDictEntry]|Dict], [RestDictEntry|ReturnList]):-
-	findHighestBigrams([Word], Dict, ReturnList).
-findHighestBigrams([Word],[[DictWord|DictWordRest]| Dict],ReturnList):-
-	findHighestBigrams([Word], Dict, Returnlist).
+%findHighestBigrams([Word],[],[]).
+%findHighestBigrams([Word], [[Word|RestDictEntry]|Dict], [RestDictEntry|ReturnList]):-
+%	findHighestBigrams([Word], Dict, ReturnList).
+%findHighestBigrams([Word],[[DictWord|DictWordRest]| Dict],ReturnList):-
+%	findHighestBigrams([Word], Dict, Returnlist).
