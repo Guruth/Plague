5bcc85e6e8b1e66eb86ba318dec8171e006a175f
Guruth <Kai.Weller90@googlemail.com>
2013-04-04 12:26:29 +0200

    added comments

--- plague.pl
+++ plague.pl
@@ -1,4 +1,5 @@
 % Word lists from : http://www.ngrams.info/
+% [1] http://www.swi-prolog.org/pldoc/package/table.html
 
 start:-
     openBigramFile(BigramHandle),
@@ -20,37 +21,48 @@ autoComplete(Sentence, BigramHandle, TrigramHandle, Return):-
     calculateProps(Bigram, Trigrams, ReturnWords),
     findHighestProp(ReturnWords,Return).
 
+% Open Tables containing  2-/3- grams using the prolog table handling mechanism (see [1])
+% Returns the File Handle used in findBi/Trigram(...)
 openBigramFile(Handle):-
     new_table(
         'wordlists/w2_.txt', 
         [count(integer), word(string), word2(string)], 
         [],
         Handle).
-
 openTrigramFile(Handle):-
     new_table(
         'wordlists/w3_.txt', 
         [count(integer), word(string), word2(string), word3(string)], 
         [],
         Handle).
-
+%Finds all matching table entries 
+%Returns a list of entries 
+%   [[Word, Word2, (Word3,) Count]|...]
 findBigram(Handle,[Word,Word2], Return):-
     findall(
         [Word, Word2, Count],
         in_table(Handle, [word(Word), word2(Word2), count(Count)], _),
         Return).
-
 findTrigrams(Handle,[Word,Word2], ReturnList):-
     findall(
         [Word, Word2, Word3, Count], 
         in_table( Handle, [word(Word), word2(Word2), word3(Word3),count(Count)], _), 
         ReturnList).
 
+%Calculates the propability based on the maximum likelihood method
+%   TrigramCount/BigramCount = Propability 
+%Returns a list of word, propability combinations
+%   [[ThirdWordOfTrigram , Propability]|...]
 calculateProps(_Sentence,[],[]).
 calculateProps([[BiWord,BiWord2,BiCount]], [[_TriWord, _TriWord2, TriWord3,TriCount]| TriList], [[TriWord3,Prop]|ReturnList]):-
     calculateProps([[BiWord,BiWord2,BiCount]],TriList,ReturnList),
     Prop is TriCount/BiCount.
 
+
+%Finds the Word with the highest propability
+%Returns the element with the highest propability
+%   [Word,Propability]
+%or, if the input list is empty, a message that no estimation is possible
 findHighestProp([],Word):-
     Word = '[Word combination not in knowledge base]'.
 findHighestProp([Word],Word).
@@ -58,12 +70,17 @@ findHighestProp([Word|RestList], Return):-
     findHighestProp(RestList,Rec),
     compareWords(Word,Rec,Return). 
 
+%Compares two word, propability combination
+%If both propabilities are equal, the first element to be compared is returned
+%Returns the word, propability combination with the highest propability
+%   [Word, Propabilitiy]
 compareWords([Word|Prop],[_CompWord,Prop],[Word|Prop]).
 compareWords([Word,Prop],[CompWord,CompProp],[ReturnWord,ReturnProp]):-
     (>(Prop,CompProp) ->
     ReturnWord = Word, ReturnProp = Prop ;
     ReturnWord = CompWord, ReturnProp = CompProp ).
 
+%Prints a list of Strings followed by a newline
 printLine([]):-
     nl.
 printLine([Sentence|Rest]):-
